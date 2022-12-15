71d7253578f79dae6c22a43ef624802a40b6261d
syimyk2 <syimyk.jetibaev99@gmail.com>
2022-12-15 11:31:50 +0845

    feat: create input by MUI component

--- src/components/UI/Checkbox.jsx
+++ src/components/UI/Checkbox.jsx
@@ -1,5 +1,12 @@
-import React from 'react'
+import * as React from 'react'
+import CheckBox from '@mui/material/Checkbox'
 
-export const Checkbox = () => {
-   return <div>Checkbox</div>
+export const Checkbox = ({ onChecked, checked }) => {
+   return (
+      <CheckBox
+         checked={checked}
+         onChange={(e) => onChecked(e.target.checked)}
+         inputProps={{ 'aria-label': 'controlled' }}
+      />
+   )
 }
