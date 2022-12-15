86bb8e7c303337b60ad138eb518cda8ddcfccd6c
syimyk2 <syimyk.jetibaev99@gmail.com>
2022-12-16 02:35:19 +0845

    feat: create signUp component and add styles for positions

---
+++ src/components/login/SignUp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import logo from '../../assets/images/mega-logo.svg'
+
+export const SignUp = () => {
+   return (
+      <div>
+         <img src={logo} alt="logo" />
+         
+      </div>
+   )
+}
