c60bfe67f73b6db96094c56af070a0ffb426ad26
syimyk2 <syimyk.jetibaev99@gmail.com>
2022-12-15 14:29:19 +0845

    fix: checkbox layout

--- src/components/UI/Checkbox.jsx
+++ src/components/UI/Checkbox.jsx
@@ -1,12 +1,25 @@
 import * as React from 'react'
 import CheckBox from '@mui/material/Checkbox'
 
-export const Checkbox = ({ onChecked, checked }) => {
+export const Checkbox = ({ checked, onChecked }) => {
    return (
       <CheckBox
+         color="secondary"
          checked={checked}
          onChange={(e) => onChecked(e.target.checked)}
-         inputProps={{ 'aria-label': 'controlled' }}
+         sx={{
+            '&:hover': {
+               '&, & + .MuiFormControlLabel-label': {
+                  color: '#7e5bc2',
+               },
+            },
+            '&.Mui-checked': {
+               '&, & + .MuiFormControlLabel-label': {
+                  color: '#7e5bc2',
+               },
+            },
+         }}
+         disableRipple
       />
    )
 }
