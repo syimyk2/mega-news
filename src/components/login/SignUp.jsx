10d78eac90db8a6782bd4d83037bfef1b2c70289
syimyk2 <syimyk.jetibaev99@gmail.com>
2022-12-17 00:34:55 +0845

    feat: add Ui card

--- src/components/login/SignUp.jsx
+++ src/components/login/SignUp.jsx
@@ -2,38 +2,41 @@ import React from 'react'
 import logo from '../../assets/images/mega-logo.svg'
 import { Flex } from '../../styles/styles-for-positions/style'
 import { Button } from '../UI/Button'
+import { Card } from '../UI/Card'
 import { Input } from '../UI/Input'
 
 export const SignUp = () => {
    const signUpHandler = () => {}
    return (
-      <form onSubmit={signUpHandler}>
-         <img src={logo} alt="logo" />
-         <Flex>
-            <Flex justify="space-between">
-               <label htmlFor="843">Фамилия</label> <Input type="text" />
+      <Card>
+         <form onSubmit={signUpHandler}>
+            <img src={logo} alt="logo" />
+            <Flex>
+               <Flex justify="space-between">
+                  <label htmlFor="843">Фамилия</label> <Input type="text" />
+               </Flex>
+               <Flex justify="space-between">
+                  <label htmlFor="843">Имя</label> <Input type="text" />
+               </Flex>
+               <Flex justify="space-between">
+                  <label htmlFor="843">Никнейм</label> <Input type="text" />
+               </Flex>
+               <Flex justify="space-between">
+                  <label htmlFor="843">Пароль</label> <Input type="password" />
+               </Flex>
+               <Flex justify="space-between">
+                  <label htmlFor="843">Подтверждение пароля</label>
+                  <Input type="password" />
+               </Flex>
             </Flex>
-            <Flex justify="space-between">
-               <label htmlFor="843">Имя</label> <Input type="text" />
-            </Flex>
-            <Flex justify="space-between">
-               <label htmlFor="843">Никнейм</label> <Input type="text" />
-            </Flex>
-            <Flex justify="space-between">
-               <label htmlFor="843">Пароль</label> <Input type="password" />
-            </Flex>
-            <Flex justify="space-between">
-               <label htmlFor="843">Подтверждение пароля</label>
-               <Input type="password" />
-            </Flex>
-         </Flex>
-         <Button>Регистрация</Button>
-         <div>
-            <p>
-               Уже есть логин?
-               <a>Войти</a>
-            </p>
-         </div>
-      </form>
+            <Button>Регистрация</Button>
+            <div>
+               <p>
+                  Уже есть логин?
+                  <a>Войти</a>
+               </p>
+            </div>
+         </form>
+      </Card>
    )
 }
