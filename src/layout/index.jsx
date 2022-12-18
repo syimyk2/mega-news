ad6fc456bfb74dc02bbc8847acc40eff01466b54
syimyk2 <syimyk.jetibaev99@gmail.com>
2022-12-19 02:39:40 +0845

    feat: add main space for dynamical render contents

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import styled from 'styled-components'
 import { Footer } from './Footer'
-// import { Header } from './Header'
+import { Header } from './Header'
 
 const Layout = () => {
    return (
       <>
-         {/* <Header /> */}
-         <Outlet />
+         <Header />
+         <Main>
+            <Outlet />
+         </Main>
          <Footer />
       </>
    )
 }
 
 export default Layout
+
+const Main = styled.main`
+   min-height: 80vh;
+`
