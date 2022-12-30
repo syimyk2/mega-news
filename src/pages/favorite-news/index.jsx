a302d8fc9accf3d789d2319aee070c27a8fc38f8
syimyk2 <syimyk.jetibaev99@gmail.com>
2022-12-31 01:41:28 +0845

    feat: create favorite news componente

--- src/pages/favorite-news/index.jsx
+++ src/pages/favorite-news/index.jsx
@@ -1,5 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
+import { NewsList } from '../../components/news-list'
+import { Title } from '../../components/UI/typography/Title'
+import { Flex } from '../../styles/styles-for-positions/style'
 
 export const FavoriteNews = () => {
-   return <div>FavoriteNews</div>
+   return (
+      <FavoriteNewsContainer>
+         <Title size="48px">Избранные новости</Title>
+         <NewsList />
+      </FavoriteNewsContainer>
+   )
 }
+
+const FavoriteNewsContainer = styled(Flex)`
+   flex-direction: column;
+`
